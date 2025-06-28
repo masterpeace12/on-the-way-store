7769e0fc7327e9a4e2752133f3421f7d3aa74a6e
masterpeace12 <ferrari1997@naver.com>
2025-06-28 17:38:36 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>On The Way Store</h1>
+      <p>배포가 성공적으로 되었습니다!</p>
+    </div>
+  );
+}
+
+export default App;
